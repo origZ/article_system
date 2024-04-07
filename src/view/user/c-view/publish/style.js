1b6bc51d8b7f26c719abcd0353675978ec6e1a46
origz <origz999@gmail.com>
2024-04-07 20:13:17 +0800

    ''

--- src/view/user/c-view/publish/style.js
+++ src/view/user/c-view/publish/style.js
@@ -1,3 +1,19 @@
 import styled from 'styled-components'
 
-export const PublishWrapper = styled.div``
+export const PublishWrapper = styled.div`
+  position: relative;
+
+  .ant-upload-list {
+  .ant-upload-list-picture-card-container,
+  .ant-upload-select {
+    width: 146px;
+    height: 146px;
+  }
+}
+
+.publish-quill {
+  .ql-editor {
+    min-height: 300px;
+  }
+}
+`
